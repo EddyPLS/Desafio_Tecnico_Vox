8d1cec5fed195e63c5fd4dfb69dc354f034a8b85
eddyclayton@gmail.com <eddyclayton@gmail.com>
2024-08-03 15:12:44 -0300

    Configuração e  testes de login

--- cypress/e2e/Login.spec.cy.js
+++ cypress/e2e/Login.spec.cy.js
@@ -1,5 +1,18 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+import Login from "../support/Login"
+
+describe('Verificar o Login', () => {
+  //Login Válido
+  it('Verificar login', () => {
+    Login.loginValido()
+  })
+
+//Login usuario inválido
+  it('Login Username Invalido', () => {
+  Login.loginUsernameInvalido()
+  })
+
+//Login senha inválido
+it('Login Password Invalido', () => {
+ Login.loginPasswordInvalido()
   })
 })
