92bd8d2e0301c650f24a59a33361f6cb88975e35
eddyclayton@gmail.com <eddyclayton@gmail.com>
2024-08-03 15:48:58 -0300

    Adicionando item ao carrinho

---
+++ cypress/e2e/AdicionarCarrinho.spec.cy.js
@@ -0,0 +1,12 @@
+import AdicionarCarrinho from "../support/AdicionarCarrinho"
+
+describe('Adicionar produtos ao Carrinho', () => {
+  //Verificar a adição de um produto ao carrinho
+  it('Adicionar produtos ao Carrinho', () => {
+   AdicionarCarrinho.adicionandoProduto()
+    
+    
+  })
+
+ })
+  
---
+++ cypress/support/AdicionarCarrinho/elements.js
@@ -0,0 +1,9 @@
+export const ELEMENTS = {
+    adicionarProduto: {
+        item: '.inventory_item_name',
+        button: '.btn_primary',
+        carrinho: '.shopping_cart_link',
+        itemCarrinho: '.cart_item'
+    }
+
+}
